7b0508355011ebe9104575694f459e754392307f
P-Chronas <72147878+P-Chronas@users.noreply.github.com>
2022-10-05 11:47:22 +1100

    Add files via upload
    Test code for Bluetooth & Gyro functionality

---
+++ test/gyro_HMCD.ino
@@ -0,0 +1,159 @@
+//
+///*
+//    MPU6050 Triple Axis Gyroscope & Accelerometer. Simple Accelerometer Example.
+//    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
+//    GIT: https://github.com/jarzebski/Arduino-MPU6050
+//    Web: http://www.jarzebski.pl
+//    (c) 2014 by Korneliusz Jarzebski
+//*/  
+//
+//#include <Wire.h>
+//#include <MPU6050.h>
+//
+//MPU6050 mpu;
+//
+//void setup() 
+//{
+//  Serial.begin(115200);
+//
+//  Serial.println("Initialize MPU6050");
+//
+//  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+//  {
+//    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+//    delay(500);
+//  }
+//
+//  // If you want, you can set accelerometer offsets
+//  // mpu.setAccelOffsetX();
+//  // mpu.setAccelOffsetY();
+//  // mpu.setAccelOffsetZ();
+//  
+//  checkSettings();
+//}
+//
+//void checkSettings()
+//{
+//  Serial.println();
+//  
+//  Serial.print(" * Sleep Mode:            ");
+//  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+//  
+//  Serial.print(" * Clock Source:          ");
+//  switch(mpu.getClockSource())
+//  {
+//    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+//    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+//    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+//    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+//    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+//    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+//    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+//  }
+//  
+//  Serial.print(" * Accelerometer:         ");
+//  switch(mpu.getRange())
+//  {
+//    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+//    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+//    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+//    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+//  }  
+//
+//  Serial.print(" * Accelerometer offsets: ");
+//  Serial.print(mpu.getAccelOffsetX());
+//  Serial.print(" / ");
+//  Serial.print(mpu.getAccelOffsetY());
+//  Serial.print(" / ");
+//  Serial.println(mpu.getAccelOffsetZ());
+//  
+//  Serial.println();
+//}
+//
+//void loop()
+//{
+//  Vector rawAccel = mpu.readRawAccel();
+//  Vector normAccel = mpu.readNormalizeAccel();
+//
+//  Serial.print(" Xraw = ");
+//  Serial.print(rawAccel.XAxis);
+//  Serial.print(" Yraw = ");
+//  Serial.print(rawAccel.YAxis);
+//  Serial.print(" Zraw = ");
+//
+//  Serial.println(rawAccel.ZAxis);
+//  Serial.print(" Xnorm = ");
+//  Serial.print(normAccel.XAxis);
+//  Serial.print(" Ynorm = ");
+//  Serial.print(normAccel.YAxis);
+//  Serial.print(" Znorm = ");
+//  Serial.println(normAccel.ZAxis);
+//  
+//  delay(1000); // Time delay between readings
+//}
+
+/*
+    MPU6050 Triple Axis Gyroscope & Accelerometer. Pitch & Roll & Yaw Gyroscope Example.
+    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
+    GIT: https://github.com/jarzebski/Arduino-MPU6050
+    Web: http://www.jarzebski.pl
+    (c) 2014 by Korneliusz Jarzebski
+*/
+
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+// Timers
+unsigned long timer = 0;
+float timeStep = 0.01;
+
+// Pitch, Roll and Yaw values
+float pitch = 0;
+float roll = 0;
+float yaw = 0;
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  // Initialize MPU6050
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+  
+  // Calibrate gyroscope. The calibration must be at rest.
+  // If you don't want calibrate, comment this line.
+  mpu.calibrateGyro();
+
+  // Set threshold sensivty. Default 3.
+  // If you don't want use threshold, comment this line or set 0.
+  // mpu.setThreshold(3);
+}
+
+void loop()
+{
+  timer = millis();
+
+  // Read normalized values
+  Vector norm = mpu.readNormalizeGyro();
+
+  // Calculate Pitch, Roll and Yaw
+  pitch = pitch + norm.YAxis * timeStep;
+  roll = roll + norm.XAxis * timeStep;
+  yaw = yaw + norm.ZAxis * timeStep;
+
+  // Output raw
+  Serial.print(" Pitch = ");
+  Serial.print(pitch);
+  Serial.print(" Roll = ");
+  Serial.print(roll);  
+  Serial.print(" Yaw = ");
+  Serial.println(yaw);
+
+  // Wait to full timeStep period
+  delay((timeStep*1000) - (millis() - timer));
+}
